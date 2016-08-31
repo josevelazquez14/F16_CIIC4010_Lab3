e7ef44854ef7575bbae2dde34f11c9ef8d5b9ae0
4015b27 <4015b27@zapote.ece.uprm.edu>
2016-08-31 09:57:25 -0400

    add an space in line 5

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("José A. Velázquez");
+		JFrame myFrame = new JFrame("José A.  Velázquez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(950, 350);
 		myFrame.setSize(200, 200);
